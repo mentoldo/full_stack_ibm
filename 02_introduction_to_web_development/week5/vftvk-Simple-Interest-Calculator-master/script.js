11c8b025450bac182608c9b86dc3ca0287bb8053
mentoldo <mentoldo>
2023-04-03 23:21:37 -0300

    Se avanzó con assignment de prueba week5

--- 02_introduction_to_web_development/week5/vftvk-Simple-Interest-Calculator-master/script.js
+++ 02_introduction_to_web_development/week5/vftvk-Simple-Interest-Calculator-master/script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = parseInt(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + years;
+    var amount = principal + interest;
+    var result = document.getElementById("result");
+
+    if (principal <= 0) {
+        alert("Please enter a positive number!");
+        document.getElementById("principal").focus();
+    }
+
+    else {
+        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>"
+        + ",\<br\>at an interest rate of " + "<mark>" + rate + "</mark>"
+        + "%.\<br\>You will receive an amount of $" + "<mark>" + amount + "</mark>"
+        + ",\<br\>In the year " + "<mark>" + year + "</mark>" 
+        + "\<br\>";
+    }
 }
 
 function updateRate() {
